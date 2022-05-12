3ecdb1e91f87b6026fa20c0864106aedc5d43f2f
Arunkumar <akayambu@cisco.com>
2022-05-12 11:29:28 -0700

    yaml fix

--- buildManifest/processCustomer.py
+++ buildManifest/processCustomer.py
@@ -14,14 +14,14 @@ destCustomerPodFile = "Customers/{0}/pod.yaml".format(args.customer)
 
 with open(customerNS, "r") as stream:
     try:
-        nsYaml = yaml.safe_load(stream)
+        nsYaml = yaml.load(stream)
         nsYaml["metadata"]["name"] = args.customer
     except Exception as exc:
         print(exc)
 
 with open(destCustomerNSFile, "w") as nsFile:
     try:
-        yaml.dump(nsYaml, nsFile)
+        yaml.dump(nsYaml, nsFile, default_flow_style=False)
     except Exception as exc:
         print(exc)
 
@@ -34,7 +34,7 @@ with open(customerPod, "r") as stream:
 
 with open(destCustomerPodFile, "w") as podFile:
     try:
-        yaml.dump(podYaml, podFile)
+        yaml.dump(podYaml, podFile, default_flow_style=False)
     except Exception as exc:
         print(exc)
 
